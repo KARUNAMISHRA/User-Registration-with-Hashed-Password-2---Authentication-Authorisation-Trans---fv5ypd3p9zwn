49760574c869a39940f5f7ed959a27206670fc74
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-05-14 11:24:56 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,5 @@
-const users   =require("../models/user.js");
-
+ const users   =require("../models/user.js");
+const bcrypt = require('bcrypt');
 
 /*
 Post request json file structure
@@ -8,8 +8,7 @@ Post request json file structure
     obj =  {
         "name":name,
         "email":email,
-        "password": password,
-        "DOB": DOB
+        "password": password
     }
 
  */
@@ -20,11 +19,31 @@ Post request json file structure
 //you will get error if user mail allready exist in that case you need to return 404 status with err message that you get.
 //to look the user schema look ../models/user.js
 
-
 const registerUser =async (req, res) => {
 
-    //write your code here
+    //Write you code here
+     const name = req.body.name;
+    const email  = req.body.email;
+    const password = req.body.password;
+    const DOB = req.body.DOB;
 
+
+    // Generate a salt to be used for hashing
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    var newuser = {
+        "name":name,
+        "email":email,
+        "password": hashedPassword,
+        "DOB":DOB
+    };
+    users.create(newuser).then((user) => {
+        res.send(user._id);
+    })
+    .catch((error) => {
+        res.status(404).send(error.message);
+    });
 }
 
 module.exports = { registerUser };
